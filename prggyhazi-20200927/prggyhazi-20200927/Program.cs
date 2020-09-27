8df07db0a5c5b40be6b63fc97e62c4a3edf903a8
Garamvölgyi Soma <68250972+garamvolgyisoma@users.noreply.github.com>
2020-09-27 21:53:35 +0200

    Add files via upload

---
+++ prggyhazi-20200927/prggyhazi-20200927/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace prggyhazi_20200927
+{
+    class Program
+    {
+        public void idobekeres()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+        
+        public void gyokvonas()
+        {
+            Console.Write("Add meg a számot, amelyiknek a gyökét akarod megkapni! (int) ");
+            int szam = int.Parse(Console.ReadLine());
+            double eredmeny = Math.Sqrt(szam);
+            Console.WriteLine(szam + " a gyök alatt = " + eredmeny);
+        }
+
+        public void emeles()
+        {
+            Console.Write("Add meg a számot! (int) ");
+            int szam = int.Parse(Console.ReadLine());
+            Console.Write("Add meg, hogy hanyadikra szeretnéd emelni! (int) ");
+            int hanyadikra = int.Parse(Console.ReadLine());
+            int eredmeny = (int)Math.Pow(szam, hanyadikra);
+            Console.WriteLine(szam + " a " + hanyadikra + "-on/en = " + eredmeny);
+        }
+
+        public void kilepes()
+        {
+
+        }
+
+        public void valaszto()
+        {
+            Console.WriteLine("Válassz funkciót!");
+            Console.WriteLine("I - Jelenlegi dátum és idő lekérdezése");
+            Console.WriteLine("G - Gyökvonás");
+            Console.WriteLine("E - Szám valahanyadikra emelése");
+            Console.WriteLine("K - Kilépés a programból");
+
+            var input = Console.ReadKey(true);
+
+            switch (input.Key)
+            {
+                case ConsoleKey.I:
+                    {
+                        idobekeres();
+                        break;
+                    }
+
+                case ConsoleKey.G:
+                    {
+                        gyokvonas();
+                        break;
+                    }
+
+                case ConsoleKey.E:
+                    {
+                        emeles();
+                        break;
+                    }
+                case ConsoleKey.K:
+                    {
+                        kilepes();
+                        break;
+                    }
+
+                default:
+                    {
+                        valaszto();
+                        break;
+                    }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.valaszto();
+        }
+    }
+}
